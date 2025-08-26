890a6968f1825fcd8a5c0a19b64ed898c6c62cfb
saurabh singh <saurabhsinghnegi71@gmail.com>
2025-08-26 10:46:17 +0530

    learn numpy arrays: created and explored arrays

---
+++ 01_array.py
@@ -0,0 +1,17 @@
+
+import numpy as np  # import numpy library
+array = np.array( [3,4,2])  # create list to array 
+print (array)
+print(type(array))
+print(np.__version__)
+
+
+# array indexing 
+marks = np.array([20,34,25,36,32])
+print(marks)
+print (marks[0]) #print o index element 
+print("print third and forth element : ",marks[3] + marks[4])
+
+
+
+
--- first.py
+++
@@ -1,5 +0,0 @@
-
-import numpy as np 
-array =np.array( [3,4,2])
-print(type(array))
-print(np.__version__)
